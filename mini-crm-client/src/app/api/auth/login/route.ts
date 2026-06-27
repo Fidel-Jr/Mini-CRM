de908f3cc8882d79ef7ac87872667cc4be22a778
Fidel <fidelsalongacolinares04@gmail.com>
2026-06-28 00:50:25 +0800

    feat: Update login route.ts - three

--- mini-crm-client/src/app/api/auth/login/route.ts
+++ mini-crm-client/src/app/api/auth/login/route.ts
@@ -1,42 +1,56 @@
 import { apiFetch } from '@/lib/api';
 import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
 
-export async function POST(req: Request) {
+export async function POST(req:Request){
+
     const body = await req.json();
 
-    const response = await apiFetch("/api/Auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body),
-    });
+    const response = await apiFetch(
+        "/api/Auth/login",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        },
+    );
 
-    if (!response.ok) {
-        // safely read body — might be empty
-        const text = await response.text();
-        const error = text ? JSON.parse(text) : { message: "Login failed" };
-        return NextResponse.json(error, { status: response.status });
-    }
 
     const result = await response.json();
 
+    if(!response.ok)
+        return Response.json(result,{status:401});
+
+
     const cookieStore = await cookies();
 
-    cookieStore.set("access_token", result.accessToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/",
-        expires: new Date(result.accessTokenExpiresAt),
+    cookieStore.set(
+        "access_token",
+        result.accessToken,
+        {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            path: "/",
+            expires: new Date(result.accessTokenExpiresAt)
+        }
+    );
+
+    cookieStore.set(
+        "refresh_token",
+        result.refreshToken,
+        {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            path: "/",
+            expires: new Date(result.refreshTokenExpiresAt)
+        }
+    );
+
+    return Response.json({
+        success:true
     });
 
-    cookieStore.set("refresh_token", result.refreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        path: "/",
-        expires: new Date(result.refreshTokenExpiresAt),
-    });
-
-    return NextResponse.json({ success: true });
 }
